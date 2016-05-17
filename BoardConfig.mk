fa79d9d4343922680d91321a3ffeee61dbac22a2
Apavayan <info@apavayan.com>
2016-05-17 15:03:58 +0530

    s5neolte: Add more Premission for sensor and hardware
    Change-Id: I1f4b150b1eee2b4786cd7ef86ab677e9ac5c86b3
    Signed-off-by: Apavayan <info@apavayan.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_OTA_ASSERT_DEVICE := s5neo3g,s5neo3gxx,s5neolte,s5neoltexx
 DEVICE_PATH := device/samsung/s5neoltexx
 
 # Include path
-TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+#TARGET_SPECIFIC_HEADER_PATH += $(DEVICE_PATH)/include
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_sec
