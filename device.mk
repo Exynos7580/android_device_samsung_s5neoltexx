7c3c84248a39a3e82eccc5983ef79ca3af137f8f
Stricted <info@stricted.net>
2017-05-21 13:03:42 +0200

    add common repo to lineage.dependencies
    Change-Id: I4d532c68cdaf3702cacafc3c8bf6e0c7831580a8

--- device.mk
+++ device.mk
@@ -123,8 +123,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
-    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(LOCAL_PATH)/configs/nfc/sec_s3fwrn7_rfreg.bin:system/etc/sec_s3fwrn7_rfreg.bin
+    $(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
     NfcNci \
