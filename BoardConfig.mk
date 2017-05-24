850295bfaf0e8eb80842636a94d614e5a0777ff1
Stricted <info@stricted.net>
2017-05-24 15:42:22 +0200

    use prebuilded dt.img
    building one on the fly causes boot issues
    
    Change-Id: I9db077b008d0f1159284cc120b642fbb2bf8405f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,14 +35,11 @@ TARGET_INIT_VENDOR_LIB := libinit_s5neolte
 TARGET_UNIFIED_DEVICE := true
 
 # Kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000KU
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SYSMAGIC000KU
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive console=tty0 androidboot.console=tty0 androidboot.hardware=samsungexynos7580 fbcon=font:VGA8x8,rotate:1 mem=471M no_console_suspend=1
 TARGET_KERNEL_CONFIG := lineageos_s5neoltexx_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/a3xelte
-BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-BOARD_KERNEL_SEPARATED_DT := true
-TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Partitions
 BOARD_HAS_NO_MISC_PARTITION:= false
