eb4534cc1b2f98ec1dac8830b258234de6193d2a
Andre Eisenbach <eisenbach@google.com>
2017-12-06 16:16:36 +0100

    Rename libnfc-brcm.conf -> libnfc-nci.conf
    Test: manual
    Merged-In: I9655a14e021024540cf342022cb817e1f8d0f9ad
    Change-Id: I9655a14e021024540cf342022cb817e1f8d0f9ad

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,7 +1,7 @@
 # NFC
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
-	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-nci.conf \
 	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
