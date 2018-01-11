c09ae52a3ece20a4a4ef5ebe0803154302cf30ca
Stricted <info@stricted.net>
2018-01-11 07:29:16 +0100

    s5neoltexx: Use hardware/amsung Audio hal
    - use default nougat audio_effects.conf to disable faulty samsung input filters
    Change-Id: I7143f5a5a147056052aead7b21cc0acb21533cec

--- device.mk
+++ device.mk
@@ -34,8 +34,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths_0.xml
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
