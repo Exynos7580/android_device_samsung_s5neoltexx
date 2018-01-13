3cf4c3b53a83b94acbf2cecce8eb0c57f1f94de1
Stricted <info@stricted.net>
2018-01-13 11:06:36 +0100

    set BOARD_CUSTOM_BOOTIMG
    Change-Id: I5ff7c60a93262fc137cd85cb064c688ebe04b0db

--- board/kernel.mk
+++ board/kernel.mk
@@ -3,6 +3,7 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 -
 #BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
 TARGET_KERNEL_CONFIG := lineageos_s5neolte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
+BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
