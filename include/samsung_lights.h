dcd143d118eeaf0cd25b605764f110231ad888b0
Stricted <info@stricted.net>
2018-01-13 13:41:46 +0100

    update include/samsung_lights.h
    Change-Id: Ida877d493187405a5702f404342d5c1447497b1f

--- include/samsung_lights.h
+++ include/samsung_lights.h
@@ -25,10 +25,10 @@
  * symlink to the locations given here, or override this header in your
  * device tree.
  */
-#define PANEL_BRIGHTNESS_NODE "/sys/devices/14800000.dsim/backlight/panel/brightness"
-#define PANEL_MAX_BRIGHTNESS_NODE "/sys/devices/14800000.dsim/backlight/panel/max_brightness"
-#define BUTTON_BRIGHTNESS_NODE "/sys/devices/virtual/sec/sec_touchkey/brightness"
-#define LED_BLINK_NODE "/sys/devices/virtual/sec/led/led_blink"
+#define PANEL_BRIGHTNESS_NODE "/sys/class/backlight/panel/brightness"
+#define PANEL_MAX_BRIGHTNESS_NODE "/sys/class/backlight/panel/max_brightness"
+#define BUTTON_BRIGHTNESS_NODE "/sys/class/sec/sec_touchkey/brightness"
+#define LED_BLINK_NODE "/sys/class/sec/led/led_blink"
 
 // Uncomment to enable variable button brightness
 //#define VAR_BUTTON_BRIGHTNESS 1
