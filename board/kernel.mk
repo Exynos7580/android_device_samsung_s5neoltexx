46c956f257eeb7d5ff4baeb6d07bee3221a5cb77
Stricted <info@stricted.net>
2018-01-14 17:22:54 +0100

    build dtb from source
    Change-Id: Id40bca64fc34b4b029f7bcd54d4879c27dec8b3b

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
 # Kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SYSMAGIC000KU
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000KU
 #BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
 TARGET_KERNEL_CONFIG := lineageos_s5neolte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
