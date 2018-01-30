27471186a47565f4b98421334f2a2560dcc42e46
Stricted <info@stricted.net>
2018-01-30 04:40:32 +0100

    build dtb from source
    this time its really working
    
    Change-Id: Ifc7c3e2bfcbbade788c8b08eef3d126442f53737

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,9 +1,12 @@
+# Boot
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := -kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000KU
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+
 # Kernel
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SYSMAGIC000KU
 #BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
 TARGET_KERNEL_CONFIG := lineageos_s5neolte_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
-BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
