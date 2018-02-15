0f2152d559cfaa05468556007fcef579d1518d5e
Paul Keith <javelinanddart@gmail.com>
2018-02-15 23:33:28 +0100

    Transition to TARGET_LD_SHIM_LIBS
     * Limits security exposure from shims
    
    see https://review.lineageos.org/#/c/206691/
    
    Change-Id: I186ed5e7331f18fa877152831b773d2ae55c2df9

---
+++ board/shims.mk
@@ -0,0 +1,2 @@
+# Shims
+TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so:/system/lib/hw/camera.universal7580.so|/vendor/lib/libshim_camera.so:/system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
