0634665c7ff644fd1d667021860d2aa507227c31
Stricted <info@stricted.net>
2018-02-18 12:57:03 +0100

    Split shims list
    Change-Id: I9c34d7968460cbfe5b5463dd2f624eea97249bc1

--- board/shims.mk
+++ board/shims.mk
@@ -1,2 +1,5 @@
 # Shims
-TARGET_LD_SHIM_LIBS := /system/bin/gpsd|/vendor/lib/libshim_gpsd.so:/system/lib/hw/camera.universal7580.so|/vendor/lib/libshim_camera.so:/system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
+TARGET_LD_SHIM_LIBS := \
+    /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
+    /system/lib/hw/camera.universal7580.so|/vendor/lib/libshim_camera.so \
+    /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
