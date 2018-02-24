b745978469e112f5de2d33fb8487e0fd6543f3c1
Stricted <info@stricted.net>
2018-02-24 20:44:12 +0100

    append TARGET_LD_SHIM_LIBS instead of overriding it
    Change-Id: I5ab3d3080fd6a30976d93cf4896e45e105e99059

--- board/shims.mk
+++ board/shims.mk
@@ -1,5 +1,5 @@
 # Shims
-TARGET_LD_SHIM_LIBS := \
+TARGET_LD_SHIM_LIBS += \
     /system/bin/gpsd|/vendor/lib/libshim_gpsd.so \
     /system/lib/hw/camera.universal7580.so|/vendor/lib/libshim_camera.so \
     /system/lib/libexynoscamera.so|/vendor/lib/libexynoscamera_shim.so
