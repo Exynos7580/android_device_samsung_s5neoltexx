a76614b31819b06570d05122a9006f301b4b06ea
Stricted <info@stricted.net>
2018-03-06 08:08:54 +0100

    commonize ramdisk
    Change-Id: I2d5fdea914de7817829b9a1777057cfa1563203d

--- board/fstab.mk
+++
@@ -1,2 +0,0 @@
-# Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.samsungexynos7580
--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,9 +1,3 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
-    fstab.samsungexynos7580 \
-    init.baseband.rc \
-    init.samsung.rc \
-    init.samsungexynos7580.rc \
-    init.samsungexynos7580.usb.rc \
-    init.wifi.rc \
-    ueventd.samsungexynos7580.rc
+    init.target.rc
