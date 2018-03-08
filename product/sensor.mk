f3ea11d06a84a8564b3dfa02a4794792c9226c1e
Stricted <info@stricted.net>
2018-03-08 02:15:11 +0100

    passthrough sensors
    Change-Id: Ia2c0f5ef72e584946278f48f1d9a15aabd4e2a31

--- product/sensor.mk
+++ product/sensor.mk
@@ -1,6 +1,5 @@
 # Sensorhub
 PRODUCT_PACKAGES += \
 	android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
 	android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
