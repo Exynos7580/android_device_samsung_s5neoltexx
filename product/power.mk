106678c0e74298d9e384d48943bd6f169246a61a
Stricted <info@stricted.net>
2018-09-15 14:34:33 +0200

    switch to new hidl based power hal
    Change-Id: I8ada84115a463850dd6111e0c22c7ef511954274

--- product/power.mk
+++ product/power.mk
@@ -1,5 +1,3 @@
 # Power
 PRODUCT_PACKAGES += \
-    power.universal7580 \
-    android.hardware.power@1.0-service \
-    android.hardware.power@1.0-impl
+    android.hardware.power@1.0-service.samsung
